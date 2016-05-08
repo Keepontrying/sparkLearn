64a743171c087377e1ae1c1308954407e3b1a05c
Keepontrying <yuwuliang@yeah.net>
2016-05-08 18:35:21 +0800

    commit

---
+++ src/main/Java/com/lyw/spark/controller/SparkLearnController.java
@@ -0,0 +1,23 @@
+package com.lyw.spark.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wangxiaowu on 16/5/8.
+ */
+@Controller
+public class SparkLearnController {
+
+    @RequestMapping("/spark")
+    @ResponseBody
+    public Map<String,Object> showSpark(){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("message", "hello spark");
+        return map;
+    }
+}
